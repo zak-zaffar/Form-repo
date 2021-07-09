70694ff0fbe7f5482a742abd6a6debf510b51b50
Zak <zakzaffar@gmail.com>
2021-07-09 20:37:03 +0100

    General added eslint ignore styling on inputs and button

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Application Form
 
 This is an application form, which uses HTML 5, CSS, JS, and PHP for the backend.
+
+## Development
+
+### Requirements
+
+- Node >10
+
+before commiting lint the project
+
+```bash
+npm run lint
+```
--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const form = document.querySelector("form#application-form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  // eslint-disable-next-line no-console
   console.log("form submit event", event);
 });
