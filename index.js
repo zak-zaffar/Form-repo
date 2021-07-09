1c947163a311a00d32781c474fe40be5f430d6c5
Zak <zakzaffar@gmail.com>
2021-07-09 11:34:17 +0100

    GEnreal finished form

---
+++ index.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('form#application-form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    console.log('form submit event', event);
+});
