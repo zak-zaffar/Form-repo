1448ef493657d9f6394e367cdaa7fc3cacc9dee6
Zak <zakzaffar@gmail.com>
2021-07-09 19:27:55 +0100

    Genreal added css added reposive stlying added linting added npn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const form = document.querySelector('form#application-form');
+const form = document.querySelector("form#application-form");
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-    console.log('form submit event', event);
+  console.log("form submit event", event);
 });
